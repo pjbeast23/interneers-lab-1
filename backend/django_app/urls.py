865dbb8b80ef73adaeef1f7e75ae111e79464153
Paras <113800166+pjbeast23@users.noreply.github.com>
2025-03-08 18:00:16 +0530

    models for a Product(Week-2)

--- backend/django_app/urls.py
+++ backend/django_app/urls.py
@@ -1,8 +1,10 @@
 # django_app/urls.py
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from django.http import JsonResponse
+from store.views import ProductViewSet
 def hello_name(request):
     """
     A simple view that returns 'Hello, {name}' in JSON format.
@@ -11,10 +13,12 @@ def hello_name(request):
     # Get 'name' from the query string, default to 'World' if missing
     name = request.GET.get("name", "World")
     return JsonResponse({"message": f"Hello, {name}!"})
-
+router = DefaultRouter()
+router.register(r'products', ProductViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', hello_name), 
+    path('', include(router.urls)),
     # Example usage: /hello/?name=Bob
     # returns {"message": "Hello, Bob!"}
 ]
