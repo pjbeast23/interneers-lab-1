add10aa78cf409b14aa6f34a6347f5753ff5697f
Paras <113800166+pjbeast23@users.noreply.github.com>
2025-03-10 23:23:46 +0530

    namechange(storesqlite)

--- backend/django_app/urls.py
+++ backend/django_app/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from django.http import JsonResponse
-from store.views import ProductViewSet
+from store-with sqlite.views import ProductViewSet
 def hello_name(request):
     """
     A simple view that returns 'Hello, {name}' in JSON format.
